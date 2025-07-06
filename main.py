324c98d9b3d594af77d7a044fe30c85b1a001b1a
Vishakha-ops <vishakhacpatil04@gmail.com>
2025-07-06 19:42:26 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+from app.schema import PatientData
+from app.model import predict_readmission
+
+app = FastAPI()
+
+@app.get("/")
+def root():
+    return {"message": "🏥 Welcome to the Hospital Readmission Predictor API"}
+
+@app.post("/predict")
+def get_prediction(data: PatientData):
+    result = predict_readmission(data)
+    return {
+        "readmitted": bool(result),
+        "risk": "High" if result else "Low"
+    }
